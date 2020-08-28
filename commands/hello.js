17dd25c7b66d8ef9b68686206ce014b81be4e2c4
WilliamAnderssonDev <williamanderssondev@gmail.com>
2020-08-28 23:26:49 +0200

    .

---
+++ commands/hello.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+	message.channel.send(`Hello ${message.member.user}! :smile:`);
+};
+
+module.exports.config = {
+	name: "hello",
+	aliases: ["hi"],
+};
